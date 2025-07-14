ab9e9899a2a8578d1c6916521e505bb07a64c0a9
River McCubbin <mccubbinri@gmail.com>
2025-07-14 16:54:29 -0400

    moved simulation to a class

--- simulation.py
+++ simulation.py
@@ -4,174 +4,152 @@ import tkinter as tk
 from multiprocessing import Pool
 import os
 
+from copy import deepcopy
+
 from planet import Planet
 from constants import *
 
-def startPlanets(
-        numPlanets: int,
-        maxVelocity: float=MAX_INITIAL_VELOCITY,
-        minMass: float=MIN_MASS,
-        maxMass: float=MAX_MASS) -> list[Planet]:
+class Simulation():
+    def __init__(self,
+        num_planets: int,
+        max_velocity: float=MAX_INITIAL_VELOCITY,
+        min_mass: float=MIN_MASS,
+        max_mass: float=MAX_MASS) -> None:
     
-    """
-    returns a list of numPlanets Planets with the following properties:
-    - random x and y coordinates
-    - initial velocities in a random direction with magnitude up to maxVelocity
-    - mass from minMass to maxMass
-    """
+        """
+        creates a Simulation with the following properties:
+        - random x and y coordinates
+        - initial velocities in a random direction with magnitude up to max_velocity
+        - mass from min_mass to max_mass
+        """
 
-    planets = []
+        self.num_planets = num_planets
+        self.planets = []
 
-    for _ in range(numPlanets):
-        # generates random on-screen coordinates for each planet
-        x = randint(0, WINWIDTH)
-        y = randint(0, WINHEIGHT)
+        for _ in range(num_planets):
+            # generates random on-screen coordinates for each planet
+            x = randint(0, WINWIDTH)
+            y = randint(0, WINHEIGHT)
 
-        # generate velocities conditioned on intialVelocity
-        xvel = random() * 2 * maxVelocity - maxVelocity
-        yvel = random() * 2 * maxVelocity - maxVelocity
+            # generate velocities conditioned on intialVelocity
+            xvel = random() * 2 * max_velocity - max_velocity
+            yvel = random() * 2 * max_velocity - max_velocity
 
-        # generates a random mass within the global variable limits
-        mass = randint(minMass, maxMass)
+            # generates a random mass within the global variable limits
+            mass = randint(min_mass, max_mass)
 
-        # store the Planet
-        planets.append(Planet(x, y, xvel, yvel, mass))
+            # store the Planet
+            self.planets.append(Planet(x, y, xvel, yvel, mass))
 
-    return planets
 
+    def collisionDetection(self):
+        """
+        Determines if any Planets in planets have collided, and combines any that
+        have.
+        """
+
+        for target_planet in self.planets:
+            # stores which planets the target planet has collided with
+            collided = []
+            for secondary_planet in self.planets:
+                # determines which planets have collided with the target
+                dist = ((target_planet.x - secondary_planet.x)**2 + (target_planet.y - secondary_planet.y)**2) ** (1/2)
+                if dist < target_planet.radius + secondary_planet.radius:
+                    collided.append(secondary_planet)
+
+            # if collisions have occurred, combines the planets and removes them
+            # from the original array
+            if len(collided) > 0:
+                x = 0
+                y = 0
+                xvel = 0
+                yvel = 0
+                mass = 0
+                # determine mass first in order to scale velocities properly later
+                for planet in collided:
+                    mass += planet.mass
+
+                # determines the rest of the parameters
+                for planet in collided:
+                    # x, y, xvel and yvel are scaled according to mass for more
+                    # "realistic" collisions
+                    x += planet.x * planet.mass / mass
+                    y += planet.y * planet.mass / mass
+                    xvel += planet.xvel * planet.mass / mass
+                    yvel += planet.yvel * planet.mass / mass
+
+                    # removes the planets once they're no longer needed for
+                    # calculations
+                    self.planets.remove(planet)
+
+                # adds the new planet to the active planets list
+                self.planets.append(Planet(x, y, xvel, yvel, mass))
+
+
+    def draw(self):
+        """
+        draws Planets planets to the tkinter canvas canvas
+        """
+
+        for planet in self.planets:
+            planet.draw(self.canvas)
+
+
+    def run(self, collisions=True):
+        """
+        Creates a tkinter canvas and simulation with num_planets Planets.
+        """
+
+        # sets up tk window etc
+        root = tk.Tk()
+        root.wm_title = ("N-body Simulation")
+        self.canvas = tk.Canvas(root, 
+                                width=WINWIDTH, 
+                                height=WINHEIGHT, 
+                                bg='black')
+        self.canvas.grid(row=0, 
+                         column=0)
+
+        # get max number of processes
+        num_processes = max(1, 
+                            min(self.num_planets // 200, 
+                                os.process_cpu_count()))
+
+        t = 0
+
+        # runs the simulation for a given number of frames
+        while True:
+            t +=1
+            print(t)
+            
+            # remove all old objects from the canvas
+            self.canvas.delete("all")
+
+            # update the planets (velocity, then position, then
+            # check for collisions, then draw)
+            with Pool(num_processes) as pool:
+                self.planets = pool.starmap(update_planet, [(planet, self.planets) for planet in self.planets])
+
+            if (collisions):
+                self.collisionDetection()
+
+            self.draw()
+
+            # update the canvas
+            self.canvas.update()
+        mainloop()
+        root.destroy()
 
 def update_planet(planet, planets):
     """
     Computes velocity and position update for Planet planet.
     """
+    planet = deepcopy(planet)
 
     planet.overallVelocity(planets)
     planet.move(WINWIDTH, WINHEIGHT)
 
-
-def overall_velocity_wrapper(planet, planets):
-    planet.overallVelocity(planets)
-
-
-def calcVelocities(planets):
-    """
-    Updates the velocities of Planets in planets based on their proximity and
-    masses.
-    """
-
-    #num_processes = len(planets) // 200
-
-    #with Pool(max(1, min(num_processes, os.process_cpu_count()))) as pool:
-    #    pool.starmap(overall_velocity_wrapper, ((planet, planets) for planet in planets))
-
-    for planet in planets:
-        planet.overallVelocity(planets)
-
-
-def movePlanets(planets, keepOnScreen=True):
-    """
-    Updates the positions of Planets in planets based on their
-    velocities.
-    """
-
-    for planet in planets:
-        # adjusts the x and y coordinates of each planet
-        planet.move(WINWIDTH, WINHEIGHT)
-
-
-def collisionDetection(planets: list[Planet]):
-    """
-    Determines if any Planets in planets have collided, and combines any that
-    have.
-    """
-
-    for target_planet in planets:
-        # stores which planets the target planet has collided with
-        collided = []
-        for secondary_planet in planets:
-            # determines which planets have collided with the target
-            dist = ((target_planet.x - secondary_planet.x)**2 + (target_planet.y - secondary_planet.y)**2) ** (1/2)
-            if dist < target_planet.radius + secondary_planet.radius:
-                collided.append(secondary_planet)
-
-        # if collisions have occurred, combines the planets and removes them
-        # from the original array
-        if len(collided) > 0:
-            x = 0
-            y = 0
-            xvel = 0
-            yvel = 0
-            mass = 0
-            # determine mass first in order to scale velocities properly later
-            for planet in collided:
-                mass += planet.mass
-
-            # determines the rest of the parameters
-            for planet in collided:
-                # x, y, xvel and yvel are scaled according to mass for more
-                # "realistic" collisions
-                x += planet.x * planet.mass / mass
-                y += planet.y * planet.mass / mass
-                xvel += planet.xvel * planet.mass / mass
-                yvel += planet.yvel * planet.mass / mass
-
-                # removes the planets once they're no longer needed for
-                # calculations
-                planets.remove(planet)
-
-            # adds the new planet to the active planets list
-            planets.append(Planet(x, y, xvel, yvel, mass))
-
-
-def drawPlanets(planets, canvas):
-    """
-    draws Planets planets to the tkinter canvas canvas
-    """
-
-    for planet in planets:
-        planet.draw(canvas)
-
-
-def runSim(numPlanets, collisions=True):
-    """
-    Creates a tkinter canvas and simulation with numPlanets Planets.
-    """
-
-    # sets up tk window etc
-    root = tk.Tk()
-    root.wm_title = ("N-body Simulation")
-    canvas = tk.Canvas(root, width=WINWIDTH, height=WINHEIGHT, bg = 'black')
-    canvas.grid(row=0, column = 0)
-
-    # creates all the initial planets
-    planets = startPlanets(numPlanets)
-
-    # runs the simulation for a given number of frames
-    while True:
-        # remove all old objects from the canvas
-        canvas.delete("all")
-
-        #num_processes = max(1, 
-        #                    min(len(planets) // 200, 
-        #                        os.process_cpu_count() - 1))
-
-        # update the planets information (velocity, then position, then check
-        # for collisions)
-        #with Pool(num_processes) as pool:
-        #    pool.starmap(update_planet, ((planet, planets) for planet in planets))
-        calcVelocities(planets)
-        movePlanets(planets)
-
-        if (collisions):
-            collisionDetection(planets)
-
-        drawPlanets(planets, canvas)
-
-        # update the canvas
-        canvas.update()
-    mainloop()
-    root.destroy()
-
+    return planet
 
 if __name__ == '__main__':
     # calls the function to run the simulation with a set time limit and
@@ -181,4 +159,5 @@ if __name__ == '__main__':
         collisions = True
     else:
         collisions = False
-    runSim(10, collisions)
+    sim = Simulation(100, collisions)
+    sim.run()
